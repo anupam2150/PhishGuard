1e9118ddf71a980a8391d0fa3765a6fcd943cb2d
Anupam <anupamshinde2775@gmail.com>
2026-05-07 03:36:52 +0530

    fix: add sudo to apt-get in build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # WeasyPrint system dependencies
-apt-get install -y libpango-1.0-0 libpangoft2-1.0-0 libpangocairo-1.0-0 \
+sudo apt-get install -y libpango-1.0-0 libpangoft2-1.0-0 libpangocairo-1.0-0 \
     libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
 
 pip install -r requirements.txt
