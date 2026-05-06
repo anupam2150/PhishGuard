1facfdf75dc7337708786e9fbc96ecf4fb0d2956
Anupam <anupamshinde2775@gmail.com>
2026-05-07 03:37:42 +0530

    fix: simplify build.sh, remove apt-get

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-# WeasyPrint system dependencies
-sudo apt-get install -y libpango-1.0-0 libpangoft2-1.0-0 libpangocairo-1.0-0 \
-    libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
-
 pip install -r requirements.txt
